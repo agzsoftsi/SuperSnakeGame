501a1ab286557d45404b7ae55cad91ba56dad342
agzsoftsi <agzsoftsi@gmail.com>
2020-01-31 05:04:02 +0000

    Create a Module SSnake v1.0

---
+++ SSnake.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Basic Module - main
+this module contain all instructions and code to create
+a classic game Skane - SuperSnake
+
+Autor: Carlos Andres Garcia Morales
+Date: 30/01/20
+E-mail: agzsoftsi@gmail.com
+Twitter: @karlgarmor
+
+"""
+
+import turtle
+""" The turtle module provides turtle graphics primitives """
+
+w = turtle.Turtle()
+# w.title("SuperSnake  karlgarmor")
+# w.bgcolor("black")
+# w.setup(width=500, height=500)
+# w.tracer(0)
